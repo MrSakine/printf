404824c7c9d9142910dbdc48c750efd400b9d161
MrSakine <msakine20@gmail.com>
2023-08-19 14:42:28 +0000

    produces output according to a format

--- _printf.c
+++ _printf.c
@@ -2,27 +2,54 @@
 
 int _printf(const char *format, ...)
 {
-        va_list args;
-        int i = 0;
+    va_list args;
+    int i = 0;
+    int len = 0;
 
-        va_start(args, format);
+    va_start(args, format);
 
-        while (format && (*(format + i)))
+    if ((format[0] == '%' && !format[1]) || !format)
+    {
+        return (-1);
+    }
+    if (format[0] == '%' && format[1] == ' ' && !format[2])
+    {
+        return (-1);
+    }
+
+    while (format && (*(format + i)))
+    {
+
+        if (*(format + i) != '%')
         {
+            _putchar(*(format + i));
+            len++;
+        }
+        else
+        {
+            format++;
 
-                if (*(format + i) == '%')
-                {
-                        _puts("");
-                }
-
-                _putchar(*(format + i));
-
-                i++;
+            if (*(format + i) == '%')
+            {
+                _putchar('%');
+                len++;
+            }
+            else if (*(format + i) == 's')
+            {
+                int n = _print_string(args);
+                len += n;
+            }
+            else if (*(format + i) == 'c')
+            {
+                _print_char(args);
+                len++;
+            }
         }
 
-        _putchar('\n');
+        i++;
+    }
 
-        va_end(args);
+    va_end(args);
 
-        return (i);
+    return (i);
 }
