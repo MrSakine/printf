26c6ff2e14d0b9a36dba80c722db1301d8ebb4d5
MrSakine <msakine20@gmail.com>
2023-08-20 21:06:01 +0000

    get length of unsigned numbers

--- main.c
+++ main.c
@@ -19,5 +19,13 @@ int main(void)
 	len2 = printf("Unknown: [%r]\n");
 	_printf("Length:[%d, %i]\n", len, len2);
 	printf("Length:[%d, %i]\n", len2, len2);
+	len = _printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%]\n");
+	_printf("Len:[%d]\n", len);
+	printf("Len:[%d]\n", len2);
+	len = _printf("Negative:[%d]\n", -762534);
+	len2 = printf("Negative:[%d]\n", -762534);
+	_printf("Len:[%d]\n", len);
+	printf("Len:[%d]\n", len2);
 	return (0);
 }
